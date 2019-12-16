c815953cf84c31d65685527794384f64b6315d2d
hewenlin01 <hewenlin01@ky-tech.com.cn>
2019-12-16 14:12:04 +0800

    init

---
+++ src/APP.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <Hello />
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  import Hello from '@/components/Hello'
+  export default {
+    name: 'app',
+    components: {
+      Hello
+    }
+  }
+</script>
